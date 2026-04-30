396e89417cfcfc8e2706c7d1dd5eb92fdffb8b53
Ridwan Hasan Khandakar <2310604@iub.edu.bd>
2026-04-30 21:18:58 +0600

    added theme button on all the pages and changed JS file.

--- js/script.js
+++ js/script.js
@@ -2,6 +2,22 @@ function toggleSemester(element) {
     element.classList.toggle("active");
 }
 
+/* THEME SYSTEM (GLOBAL + SAVED) */
 function toggleTheme() {
     document.body.classList.toggle("dark");
+
+    if (document.body.classList.contains("dark")) {
+        localStorage.setItem("theme", "dark");
+    } else {
+        localStorage.setItem("theme", "light");
+    }
 }
+
+/* LOAD THEME ON EVERY PAGE */
+window.onload = function () {
+    const savedTheme = localStorage.getItem("theme");
+
+    if (savedTheme === "dark") {
+        document.body.classList.add("dark");
+    }
+};
